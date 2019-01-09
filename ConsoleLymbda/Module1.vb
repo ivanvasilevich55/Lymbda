6ed1e5c87e81d31b572bca3cebe07e1d59b2108f
Ivan <ivan@yandex.ru>
2019-01-09 10:50:42 +0300

    go

---
+++ ConsoleLymbda/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+	Sub Main()
+		'Можно также объявить и вызвать лямбда-выражение, как показано в следующем примере.
+		Console.WriteLine((Function(num As Integer) num + 1)(5))
+		Console.ReadLine()
+	End Sub
+
+End Module
---
+++ LymbdaExpresion/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+	'сигнатура
+	Delegate Function delegateType() As String
+
+	Private s As String
+	Private Sub start() Handles Me.Load
+		'безимянная функция
+		Dim a As delegateType = Function()
+									Return "Hello"
+								End Function
+		Me.Text = a()
+		Dim increment = Sub(x As Integer) Me.Text = x.ToString
+		increment(4)
+		Dim increment2 = Function(x As Integer) x + 1
+		Me.Text = increment2(7).ToString
+	End Sub
+
+End Class
